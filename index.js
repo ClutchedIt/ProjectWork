2d8b4e16d9d127f24168d3ce8c4ad93e2d71f3ea
Kris ITS <krisits@gmail.com>
2022-06-17 14:48:08 +0200

    Finished Kris part on second page of FrontEnd

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 window.onload = () => {
-    const sideMenu = document.querySelector("aside");
-    const menuBtn = document.querySelector("#menu-btn");
-    const closeBtn = document.querySelector("#close-btn");
-    const themeToggler = document.querySelector(".theme-toggler");
+    document.read
+  const sideMenu = document.querySelector("aside");
+  const menuBtn = document.querySelector("#menu-btn");
+  const closeBtn = document.querySelector("#close-btn");
+  const themeToggler = document.querySelector(".theme-toggler");
 
-    //open sidemenu
-    menuBtn.addEventListener('click', () => {
-        sideMenu.style.display = 'block';
-    })
+  //open sidemenu
+  menuBtn.addEventListener("click", () => {
+    sideMenu.style.display = "block";
+  });
 
-    //close sidemenu
-    closeBtn.addEventListener('click', () => {
-        sideMenu.style.display = 'none';
-    })
+  //close sidemenu
+  closeBtn.addEventListener("click", () => {
+    sideMenu.style.display = "none";
+  });
 
-    //change theme
-    themeToggler.addEventListener('click', () => {
-        document.body.classList.toggle('dark-theme-variables');
+  //change theme
+  themeToggler.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme-variables");
 
-        themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
-        themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
-        //themeToggler.querySelector('span').classList.toggle('active');
-    })
-}
+    themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+    themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+    //themeToggler.querySelector('span').classList.toggle('active');
+  });
+};
