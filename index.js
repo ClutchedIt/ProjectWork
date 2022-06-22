368d26f8665631697a5c3b2bb6221fdb0feefe11
Kris ITS <krisits@gmail.com>
2022-06-22 15:53:08 +0200

    Finished for Andrea

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ window.onload = () => {
   const closeBtn = document.querySelector("#close-btn");
   const themeToggler = document.querySelector(".theme-toggler");
   const floorsDropdown = document.querySelector("#floorsDropdown");
-  const floorsDropdownParent = document.getElementById('floorsDropdownParent')
+  const floorsDropdownParent = document.getElementById('floorsDropdownParent');
   let floorsDropdownState = false
+  const logo = document.getElementById('logo');
+  const logon = document.getElementById('logon');
+  let showLogon = false;
+
 
   floorsDropdownParent.addEventListener('click', () => {
     if(!floorsDropdownState) {
@@ -34,7 +38,15 @@ window.onload = () => {
   //change theme
   themeToggler.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme-variables");
-
+    if(!showLogon){
+      logo.style.display = "none";
+      logon.style.display = "block";
+      showLogon = true;
+    }else{
+      logo.style.display = "block";
+      logon.style.display = "none";
+      showLogon = false;
+    }
     themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
     themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
     //themeToggler.querySelector('span').classList.toggle('active');
